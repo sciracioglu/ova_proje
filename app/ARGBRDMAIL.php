0031a5e4cc95b247e844d23b68657f8eda4bf3e0
Serkan Ciracioglu <serkan@tedankara.k12.tr>
2020-03-02 21:09:41 +0300

    personel db ayari

--- app/ARGBRDMAIL.php
+++ app/ARGBRDMAIL.php
@@ -7,4 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class ARGBRDMAIL extends Model
 {
     protected $table      = 'ARGBRDMAIL';
+    protected $connection = 'personel';
 }
--- config/database.php
+++ config/database.php
@@ -47,6 +47,16 @@ return [
             'charset'  => 'utf8',
             'prefix'   => '',
         ],
+        'sqlsrv' => [
+            'driver'   => 'sqlsrv',
+            'host'     => env('DB_HOST', 'localhost'),
+            'port'     => env('DB_PORT', '1433'),
+            'database' => env('PER_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
+            'charset'  => 'utf8',
+            'prefix'   => '',
+        ],
     ],
 
     /*
